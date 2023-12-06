ca181d6710b889d643f2fc1c436a24f17778fa3e
Danijel Dedic <dedic.d@gmail.com>
2023-12-06 16:05:39 +0100

    hack the index file

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 var fs = require('fs')
 var path = require('path')
+var os = require('os')
 
-var pathFile = path.join(__dirname, 'path.txt')
+function getPlatformPath () {
+    var platform = process.env.npm_config_platform || os.platform()
+
+    switch (platform) {
+      case 'darwin':
+        return 'dist/Electron.app/Contents/MacOS/Electron'
+      case 'freebsd':
+      case 'linux':
+        return 'dist/electron'
+      case 'win32':
+        return 'dist/electron.exe'
+      default:
+        throw new Error('Electron builds are not available on platform: ' + platform)
+    }
+}
+
+var platformPath = getPlatformPath()
 
 if (fs.existsSync(pathFile)) {
-  module.exports = path.join(__dirname, fs.readFileSync(pathFile, 'utf-8'))
+  module.exports = path.join(__dirname, platformPath)
 } else {
   throw new Error('Electron failed to install correctly, please delete node_modules/electron and try installing again')
 }
