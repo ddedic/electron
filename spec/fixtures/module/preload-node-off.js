0b5eaeeb45fe3ca0fd86ce6a1ec6333e6845ee3d
Danijel Dedic <dedic.d@gmail.com>
2023-12-06 15:48:56 +0100

    Add compiled versions

--- spec/fixtures/module/preload-node-off.js
+++
@@ -1,7 +0,0 @@
-setImmediate(function () {
-  try {
-    console.log([typeof process, typeof setImmediate, typeof global, typeof Buffer].join(' '))
-  } catch (e) {
-    console.log(e.message)
-  }
-})
